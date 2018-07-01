490d184c03c11003f1635bf914aa76734c5ec70e
Richard Elkins <richard.elkins@gmail.com>
2018-07-01 14:05:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
-from setuptools import setup
+from setuptools import *
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-setup(name="pycloaking",
+setup(
+    name="pycloaking",
     version="1.0",
-    description="Cloak/Uncloak Sensitive Files",
-    long_description=readme(),
     url="https://github.com/texadactyl/pycloaking",
     author="Richard Elkins",
     author_email="richard.elkinsr@gmail.com",
-	license="MIT",
-    packages=["pycloaking"],
+    description="Cloak/Uncloak Sensitive Files",
+    long_description=open('README.md').read(),
+    packages=find_packages(),
 	install_requires=['pycrypto'],
-    zip_safe=False)
-
+)
