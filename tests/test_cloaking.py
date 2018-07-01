43df9925229ea61ca57a2e21ac7312810671374d
Richard Elkins <richard.elkins@gmail.com>
2018-07-01 13:22:16 -0500

    Add files via upload

---
+++ tests/test_cloaking.py
@@ -0,0 +1,22 @@
+from os.path import getsize
+from pycloaking import *
+
+if __name__ == "__main__": # stand-alone self-imposed test
+
+    PASSWORD = "Mary Had a Little Lamb"
+    CLEARTEXT_FILE_1 = "/tmp/nil"
+    CLEARTEXT_FILE_1 = "/etc/hosts"
+    CLEARTEXT_FILE_1 = "/usr/bin/pandoc"
+    CIPHERTEXT_FILE = "/tmp/cloaked"
+    CLEARTEXT_FILE_2 = "/tmp/uncloaked.txt"
+    et = cloak_file(PASSWORD, CLEARTEXT_FILE_1, CIPHERTEXT_FILE)
+    print("cloak_file elapsed time(s) = %.3f" %et)
+    et = uncloak_file(PASSWORD, CIPHERTEXT_FILE, CLEARTEXT_FILE_2)
+    print("uncloak_file elapsed time(s) = %.3f" %et)
+    filesize1 = getsize(CLEARTEXT_FILE_1)
+    print("Original file size:", filesize1)
+    cloakedsize = getsize(CIPHERTEXT_FILE)
+    print("Cloaked file size:", cloakedsize)
+    filesize2 = getsize(CLEARTEXT_FILE_2)
+    print("Uncloaked file size:", filesize2)
+
