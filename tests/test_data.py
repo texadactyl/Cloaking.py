8bc0b3b55688993729e08b3a8e96d5ea0da05ec2
Richard Elkins <richard.elkins@gmail.com>
2023-06-15 11:08:34 -0500

    delete extraneous file

--- tests/test_data.py
+++
@@ -1,8 +0,0 @@
-from os import path
-
-here = path.dirname(path.abspath(__file__))
-
-CLEARTEXT_FILE_1 = path.join(here, 'test_data/cleartext_1.txt')
-CLEARTEXT_FILE_2 = path.join(here, 'test_data/cleartext_2.txt')
-CIPHERTEXT_FILE = path.join(here, 'test_data/ciphertext.tif')
-
